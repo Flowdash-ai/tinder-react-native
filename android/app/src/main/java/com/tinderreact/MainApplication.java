e404828d9719804a88cffd3d829905633725b1f3
Jatin Sandilya <sandilya.jatin@gmail.com>
2020-05-27 22:24:30 +0530

    Integrate via npm

--- android/app/src/main/java/com/tinderreact/MainApplication.java
+++ android/app/src/main/java/com/tinderreact/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new FlowdashNativePackage(getResources().getString(R.string.FlowdashAppId),getResources().getBoolean(R.bool.FlowdashIncludeEditor)),
+            new FlowdashNativePackage(getResources().getString(R.string.FlowdashAppId),getResources().getString(R.string.FlowdashAppKey),getResources().getBoolean(R.bool.FlowdashIncludeEditor)),
             new RNGestureHandlerPackage()
       );
     }
